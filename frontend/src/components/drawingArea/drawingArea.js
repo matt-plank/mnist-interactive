b727d7f8ea85646da251018527ce8846b20d2d4c
matt-plank <48247883+matt-plank@users.noreply.github.com>
2022-12-16 12:20:33 +0000

    Made canvas interactive

--- frontend/src/components/drawingArea/drawingArea.js
+++ frontend/src/components/drawingArea/drawingArea.js
@@ -1,10 +1,43 @@
+import { useRef } from "react";
 import "./drawingArea.css";
 
 const DrawingArea = () => {
+  const canvasRef = useRef(null);
+
+  const drawAt = (ctx, x, y, colour) => {
+    const cellSize = 50;
+
+    const cellX = Math.floor(x / cellSize) * cellSize;
+    const cellY = Math.floor(y / cellSize) * cellSize;
+
+    ctx.strokeStyle = colour;
+
+    ctx.fillRect(cellX, cellY, cellSize, cellSize);
+  };
+
+  const leftClickOnCanvas = (event) => {
+    if (!(canvasRef && canvasRef.current)) return;
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    const canvasRect = canvas.getBoundingClientRect();
+    const clickX = event.clientX - canvasRect.left;
+    const clickY = event.clientY - canvasRect.top;
+
+    drawAt(ctx, clickX, clickY);
+  };
+
   return (
     <>
       <div className="display-wrapper">
-        <canvas id="display-area"></canvas>
+        <canvas
+          ref={canvasRef}
+          onClick={leftClickOnCanvas}
+          id="display-area"
+          height={500}
+          width={500}
+        ></canvas>
       </div>
     </>
   );
